78f8f38f8959736722019d8a424392a3b907b151
RuslanSalimov <mail-spam2012@yandex.ru>
2021-12-14 12:45:55 +0500

    Create get and set methods

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-const form = document.querySelector('.todo-control');
-const todoList = document.querySelector('.todo-list');
-const todoCompleted = document.querySelector('.todo-completed');
 
-class myTodo{
+
+class myTodo {
     constructor({
-        formSelector = '.todo-control',
-
+                    formSelector = '.todo-control',
+                    todoListSelector = '.todo-list',
+                    todoCompletedSelector = '.todo-completed',
+                    formInputSelector = '.header-input',
                 }) {
+        try {
+            this.form = document.querySelector(formSelector);
+            this.todoList = document.querySelector(todoListSelector);
+            this.todoCompleted = document.querySelector(todoCompletedSelector);
+            this.formInput = this.form.querySelector(formInputSelector);
+            this.toDos = [];
+            this.init()
+        } catch (error){
+            console.warn(new Error(error))
+        }
     }
+    init(){
+        this.getFromStorage();
+    }
+    setToStorage(){
+        localStorage.setItem('todo22', JSON.stringify(this.toDos))
+    }
+    getFromStorage(){
+        const todos = JSON.parse(localStorage.getItem('todo22'));
+        this.toDos = todos.length > 0 ? todos : [];
+    }
+    render(){
+
+    }
+
 }
+const todo = new myTodo({});
