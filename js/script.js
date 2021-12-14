436856b2d29a855a4649014c84297a115a5795b4
RuslanSalimov <mail-spam2012@yandex.ru>
2021-12-14 12:27:54 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.todo-control');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+class myTodo{
+    constructor({
+        formSelector = '.todo-control',
+
+                }) {
+    }
+}
